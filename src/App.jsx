be207f2dd3ca8529c8ef29a5968a2bc3c83ac9ef
LucassSantoss <lucascross.pereirasantos@gmail.com>
2025-04-10 16:56:20 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header>
+        <h1>Hello world</h1>
+      </header>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque fuga accusamus dolorum magnam delectus quos ab modi quas, iusto soluta. Asperiores deleniti sapiente porro accusamus commodi unde a provident!</p>
+    </>
+  )
+}
+
+export default App
